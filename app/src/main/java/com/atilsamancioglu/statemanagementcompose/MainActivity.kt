95556dc32877bbb14330d70c44b10e41cb56bf27
Atil Samancioglu <samancioglu.atil@gmail.com>
2024-11-07 13:45:09 +0300

    textfield introduced

--- app/src/main/java/com/atilsamancioglu/statemanagementcompose/MainActivity.kt
+++ app/src/main/java/com/atilsamancioglu/statemanagementcompose/MainActivity.kt
@@ -12,9 +12,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -53,6 +55,14 @@ fun MainScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        var kullaniciGirdisi = "Yaşınızı Giriniz"
+
+        TextField(value = kullaniciGirdisi, onValueChange = {
+            kullaniciGirdisi = it
+            println(it)
+        })
+        Spacer(modifier = Modifier.padding(10.dp))
         Text(text = "Merhaba Android")
         Spacer(modifier = Modifier.padding(10.dp))
         Button(onClick = {
